9ba736dea68fb4757df2ef42fe35e51d300123f3
Ravoxsg <mathieu.ravaut@student.ecp.fr>
2018-03-10 16:07:10 -0500

    cosinesim

--- generate_random_pair.py
+++ generate_random_pair.py
@@ -1,4 +1,5 @@
 # Gives a certain number of subreddits all distinct and containing a minimum number of threads
+# Also passes these subreddits names to a word2vec embeddings to get their cosine similarity with a given subreddit name. 
 
 import numpy as np 
 import os
@@ -11,15 +12,19 @@ import gc
 import bz2
 import json
 import pickle
+import gensim
+from sklearn.metrics.pairwise import cosine_similarity
 
 data_path = 'E:/bz2_files/' # where are the bz2 files?
-starting_year = 2016
+starting_year = 2006
 starting_month = 1
-ending_year = 2016
-ending_month = 10
-min_threads = 50
-nb_of_subr = 6
+ending_year = 2006
+ending_month = 3
+min_threads = 5
+nb_of_subr = 20
+model = gensim.models.KeyedVectors.load_word2vec_format('C:/Users/mathi/Documents/DATA SCIENCE/KAGGLE/Toxic_Kaggle/GoogleNews-vectors-negative300.bin', binary=True)
 
+# grab all subreddits in the given period
 def screen_threads(data_path, starting_year, starting_month, ending_year, ending_month):
 
     os.chdir(data_path)
@@ -48,24 +53,53 @@ def screen_threads(data_path, starting_year, starting_month, ending_year, ending
                             continue
     return subreddits
 
+# randomly select distinct subreddits that have enough threads
 def random_pair(subreddits):
+
+    if (nb_of_subr >= len(subreddits)):
+        print("WARNING: you are trying to grab more subreddits than there actually are")
 
     keys = list(subreddits.keys())
 
     indices = []
     names = []
 
-    for r in range(nb_of_subr):
+    attempts = 0
+    while (attempts <= 3*len(subreddits)) and (len(indices) < nb_of_subr):
         i = np.random.randint(len(keys))
+        attempts += 1
         while not(subreddits[keys[i]] >= min_threads) or (i in indices):
             i = np.random.randint(len(keys))
+            attempts += 1
         indices.append(i)
         names.append(keys[i])
 
     return names
 
+# compute the cosine similarity between these subreddits names and a given subreddit under study 
+# returns a dictionary whose keys are subreddit names and values cosine similarities
+def similarity(pair_element, names):
+
+
+    ref = model[pair_element]
+    similarities = {}
+
+    for name in names:
+        try:
+            embedding = model[name]
+            similarities[name] = cosine_similarity(ref.reshape((1,300)), embedding.reshape((1,300)))[0][0]
+        except KeyError:
+            similarities[name] = 'Subreddit name not recognized by the word2vec model'
+
+    return similarities
+
+
 if __name__ == '__main__':
 
     subreddits = screen_threads(data_path, starting_year, starting_month, ending_year, ending_month)
-    indices = random_pair(subreddits)
-    print(indices)
+    names = random_pair(subreddits)
+    print(names)
+    similarities = similarity('prolife', names)
+    print(similarities)
+    print(cosine_similarity(model['prolife'].reshape((1,300)), model['prochoice'].reshape((1,300)))[0][0])
+    print(cosine_similarity(model['democrats'].reshape((1,300)), model['republicans'].reshape((1,300)))[0][0])
