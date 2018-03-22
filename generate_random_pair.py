fc04adbf8d8846dbc9dec1899fa40fafa0d2bc4f
Ravoxsg <mathieu.ravaut@student.ecp.fr>
2018-03-22 11:36:35 -0400

    nlp on subreddit names

--- generate_random_pair.py
+++ generate_random_pair.py
@@ -14,19 +14,54 @@ import json
 import pickle
 import gensim
 from sklearn.metrics.pairwise import cosine_similarity
+import re
+import nltk
+
 
 data_path = 'E:/bz2_files/' # where are the bz2 files?
 home_path = 'C:/Users/mathi/Documents/ETUDES/4-University of Toronto/WINTER/3-Topics in CSS/3_Project/Code/subreddit names/'
 starting_year = 2016
 starting_month = 1
 ending_year = 2016
-ending_month = 6
-min_threads = 60
-nb_of_subr = 300
+ending_month = 1
+min_threads = 100*(ending_month - starting_month + 1)
+nb_of_subr = 50
 model = gensim.models.KeyedVectors.load_word2vec_format('C:/Users/mathi/Documents/DATA SCIENCE/KAGGLE/Toxic_Kaggle/GoogleNews-vectors-negative300.bin', binary=True)
 random_thres = 0.025
 similar_thres = 0.4
+attempts_coeff = 4
 
+
+# process the name of each subreddit
+def preproc(name):
+    # 1: remove underscores
+    temp = name.replace('_','')
+    # 2: remove numbers
+    temp = re.sub(r'[0-9]+', '', temp)
+    # 3: split on capital letters
+    split = re.findall('[A-Z][^A-Z]*', temp)
+    if split == []:
+        split = [temp]
+    # 4: part-of-speech tagging to identify proper nouns
+    tagged_sent = nltk.pos_tag(split)
+    vectors = []
+    for i in range(len(tagged_sent)):
+        if tagged_sent[i][1] == 'NNP':
+            try:
+                embedding = model[tagged_sent[i][0]]
+                vectors.append(embedding)
+            except KeyError:
+                continue
+        else:
+            try:
+                embedding = model[tagged_sent[i][0].lower()]
+                vectors.append(embedding)
+            except KeyError:
+                continue            
+    if vectors == []:
+        return 'Subreddit name not recognized by the word2vec model'
+    else:
+        return np.mean(np.array(vectors), axis=0)
 
 # grab all subreddits in the given period
 def screen_threads(data_path, starting_year, starting_month, ending_year, ending_month):
@@ -69,7 +104,7 @@ def random_subs(subreddits, min_threads, nb_of_subr):
     names = []
 
     attempts = 0
-    while (attempts <= 4*len(keys)) and (len(indices) < nb_of_subr):
+    while (attempts <= attempts_coeff*len(keys)) and (len(indices) < nb_of_subr):
         #print(attempts)
         i = np.random.randint(len(keys))
         attempts += 1
@@ -86,15 +121,15 @@ def random_subs(subreddits, min_threads, nb_of_subr):
 # returns a dictionary whose keys are subreddit names and values cosine similarities
 def group_similarity(pair_element, names):
 
-    ref = model[pair_element]
+    ref = prepoc(pair_element)
     similarities = {}
 
     for name in names:
-        try:
-            embedding = model[name]
+        embedding = preproc(name)
+        if type(embedding) != str:
             similarities[name] = cosine_similarity(ref.reshape((1,300)), embedding.reshape((1,300)))[0][0]
-        except KeyError:
-            similarities[name] = 'Subreddit name not recognized by the word2vec model'
+        else:
+            similarities[name] = embedding
 
     return similarities
 
@@ -108,26 +143,28 @@ def similarities(names, home_path):
     similar_pairs = []
 
     for name in names:
-        try:
-            embedding = model[name]
+        embedding = preproc(name)
+        if type(embedding) != str:
             similarities[name] = {}
             for other_name in names:
                 if other_name != name:
-                    try:
-                        other_embedding = model[other_name]
+                    other_embedding = preproc(other_name)
+                    if type(other_embedding) != str:
                         cos = cosine_similarity(embedding.reshape((1,300)), other_embedding.reshape((1,300)))[0][0]
                         similarities[name][other_name] = cos
                         if abs(cos) <= random_thres:
-                            random_pairs.append([name,other_name])
+                            if not([other_name, name] in random_pairs):
+                                random_pairs.append([name,other_name])
                         if abs(cos) >= similar_thres:
-                            similar_pairs.append([name,other_name])
-                    except KeyError:
-                        similarities[name][other_name] = 'Subreddit name not recognized by the word2vec model' 
-        except KeyError:
-            similarities[name] = 'Subreddit name not recognized by the word2vec model'
+                            if not([other_name, name] in similar_pairs):
+                                similar_pairs.append([name,other_name])
+                    else:
+                        similarities[name][other_name] = other_embedding
+        else:
+            similarities[name] = embedding
 
     # write similarities dico
-    with open('similarities_{}_{}_to_{}_{}.csv'.format(starting_year, starting_month, ending_year, ending_month), 'w') as file:
+    with open('similarities_{}_{}_to_{}_{}_{}_subs_{}_per_month.csv'.format(starting_year, starting_month, ending_year, ending_month, nb_of_subr, int(min_threads/(ending_month-starting_month+1))), 'w') as file:
         file.write("subreddit"+','+"subreddit"+"\n")
         for key in similarities.keys():
             file.write(key+"\n")
@@ -137,14 +174,14 @@ def similarities(names, home_path):
     print('We finished writing similarities between all subreddits')
 
     # write random pairs
-    with open('random_pairs_{}_{}_to_{}_{}.csv'.format(starting_year, starting_month, ending_year, ending_month), 'w') as file:
+    with open('random_pairs_{}_{}_to_{}_{}_{}_subs_{}_per_month.csv'.format(starting_year, starting_month, ending_year, ending_month, nb_of_subr, int(min_threads/(ending_month-starting_month+1))), 'w') as file:
          file.write("subreddit 1"+','+"subreddit 2"+"\n")
          for pair in random_pairs:
             file.write(pair[0]+','+pair[1]+'\n')
     print('We finished writing random pairs')
 
     # write similar pairs
-    with open('similar_pairs_{}_{}_to_{}_{}.csv'.format(starting_year, starting_month, ending_year, ending_month), 'w') as file:
+    with open('similar_pairs_{}_{}_to_{}_{}_{}_subs_{}_per_month.csv'.format(starting_year, starting_month, ending_year, ending_month, nb_of_subr, int(min_threads/(ending_month-starting_month+1))), 'w') as file:
          file.write("subreddit 1"+','+"subreddit 2"+"\n")
          for pair in similar_pairs:
             file.write(pair[0]+','+pair[1]+'\n')
