e906d18b76f3af881a133c761c98094be99c31ca
Ravoxsg <mathieu.ravaut@student.ecp.fr>
2018-03-28 02:13:53 -0400

    split into 2 scripts

--- process_threads.py
+++ process_threads.py
@@ -1,7 +1,7 @@
-# Utility functions 
+# Utility functions for threads
 
 import numpy as np 
-import os
+import f
 import csv
 import ast 
 from tqdm import tqdm
@@ -15,8 +15,6 @@ from sklearn.metrics.pairwise import cosine_similarity as cos
 
 import pdb
 
-from nlp_utils import preproc
-from generate_random_pair import random_thres
 
 # merge threads from the same subreddit over different months
 def merge_threads(subreddit, starting_year, starting_month, ending_year, ending_month):
@@ -71,50 +69,4 @@ def merge_threads(subreddit, starting_year, starting_month, ending_year, ending_
 
     return unique_threads
 
-# stores each control group 
-def pairs_index():
 
-    clashing_elements = ["prolife", "prochoice", "The_Donald", "HillaryForAmerica", "Feminism", "TheRedPill",
-    "climatechange", "climateskeptics"]
-
-    clashing_pairs = [["prolife", "prochoice"], ["The_Donald", "HillaryForAmerica"],
-    ["Feminism", "TheRedPill"], ["climatechange","climateskeptics"]]
-
-    non_clashing_elements = ["prohealth", "2016_elections", "Anarchism", "PurplePillDebate", "climate", "fastfood",
-    "FoodFans","Calligraphy","bookbinding"]
-
-    similar_pairs = [["prolife","prohealth"], ["prolife","prohealth"],
-    ["The_Donald", "2016_elections"], ["HillaryForAmerica", "2016_elections"],
-    ["Feminism", "Anarchism"], ["TheRedPill", "PurplePillDebate"],
-    ["climatechange","climate"], ["climateskeptics", "climate"]]
-
-    random_pairs = []
-    n = len(clashing_elements)
-    for i in range(n):
-        j = np.random.randint(n)
-        while i == j:
-            j = np.random.randint(n)
-        random_pairs.append([clashing_elements[i], similar_pairs[j][1]])
-
-    control_similar_pairs = [["fastfood","FoodFans"], ["Calligraphy", "bookbinding"]]
-
-    control_random_pairs = []
-    m = len(non_clashing_elements)
-    for i in range(m):
-        for j in range(i+1,m):
-            if (type(preproc(non_clashing_elements[i])) != str) and (type(preproc(non_clashing_elements[j])) != str):
-                if abs(cos(preproc(non_clashing_elements[i]).reshape((1,300)), preproc(non_clashing_elements[j]).reshape((1,300)))[0][0]) < random_thres:
-                    control_random_pairs.append([non_clashing_elements[i], non_clashing_elements[j]])
-
-    all_pairs = {"conflict": clashing_pairs, "similar": similar_pairs,
-    "random": random_pairs, "control_similar": control_similar_pairs, "control_random": control_similar_pairs}
-
-    print(all_pairs)
-
-    with open('pairs_index.pickle', 'wb') as handle:
-        pickle.dump(all_pairs, handle)
-
-
-if __name__ == '__main__':
-
-    pairs_index()
